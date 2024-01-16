3013cc57328fb0145da0723f40cfbd53658a30ac
Hendrix1220 <156188254+Hendrix1220@users.noreply.github.com>
2024-01-16 17:43:16 +0000

    Add files via upload

---
+++ PYTHON/Independent Study Practice/morse_code.py
@@ -0,0 +1,50 @@
+def morse_translator(text):
+    # Morse code mapping
+    morse_code_dict = {
+        "A": ".-",
+        "B": "-...",
+        "C": "-.-.",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "V": "...-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+    }
+
+    # Your code goes here
+    text=text.upper()
+    morse_result=" "
+    for char in text:
+        if char!=" ":
+            morse_result=morse_result+morse_code_dict[char]
+        else:
+            morse_result=morse_result+"/"
+    return morse_result.strip()
+
+
+# Test cases
+print(
+    morse_translator("HELLO WORLD")
+)  # Expected output: ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
+print(morse_translator("Python"))  # Expected output: ".--. -.-- - .... --- -."
+print(
+    morse_translator("Morse Code")
+)  # Expected output: "-- --- .-. ... . / -.-. --- -.. ."
